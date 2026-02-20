479e6a18e8a09eb9bcd9792377090bab3eb9947c
KochubeevAndrei <manualcheg@yandex.ru>
2026-02-20 16:35:19 +0300

    Login screen - 2st try to set up

--- composeApp/src/commonMain/kotlin/com/manualcheg/ktscourse/App.kt
+++ composeApp/src/commonMain/kotlin/com/manualcheg/ktscourse/App.kt
@@ -7,30 +7,4 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     AppNavHost()
-
-
-    /*var showContent by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .safeContentPadding()
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello!")
-
-        Button(onClick = { showContent = !showContent }) {
-            Text("Click me!")
-        }
-        AnimatedVisibility(showContent) {
-            val greeting = remember { Greeting().greet() }
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-                Text("Compose: $greeting")
-            }
-        }
-    }*/
 }
--- composeApp/src/commonMain/kotlin/com/manualcheg/ktscourse/ui/screens/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/manualcheg/ktscourse/ui/screens/LoginScreen.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextObfuscationMode.Companion.RevealLastTyped
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecureTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -30,18 +35,18 @@ fun LoginScreen() {
             TextField(
                 state = rememberTextFieldState(""),
                 modifier = Modifier.padding(start = 40.dp, end = 40.dp),
-                label = {Text("Email")}
+                label = { Text("Email") },
+                lineLimits = TextFieldLineLimits.SingleLine,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
             )
-            TextField(
+            SecureTextField(
                 state = rememberTextFieldState(""),
                 modifier = Modifier.padding(start = 40.dp, end = 40.dp),
-                label = {Text("Password")}
-                //                inputType = ,
-//                placeholder = { Text("Password") }
-//                type = TextFieldType.Password
+                label = { Text("Password") },
+                textObfuscationMode = RevealLastTyped
             )
             Button(
-                onClick = {},
+                onClick = { },
             ) {
                 Text("Login")
             }
